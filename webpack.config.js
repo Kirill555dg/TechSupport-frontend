5bc624ce1063bc03bffaa30398089ff9b408e3c8
Kerrodar <Kirill555dg@gmail.com>
2024-12-07 10:17:29 +0300

    renamed index

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     entry: {
-      index: path.resolve(__dirname, 'src', 'scripts', 'index.js'),
+      new_task: path.resolve(__dirname, 'src', 'scripts', 'new_task.js'),
       login: path.resolve(__dirname, 'src', 'scripts', 'login.js'),
       support: path.resolve(__dirname, 'src', 'scripts', 'support.js'),
     },
@@ -45,9 +45,9 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-          filename: 'index.html',
-          template: path.resolve(__dirname, 'src', 'index.html'),
-          chunks: ['index'], // Указываем, какой JS использовать
+          filename: 'new_task.html',
+          template: path.resolve(__dirname, 'src', 'new_task.html'),
+          chunks: ['new_task'], // Указываем, какой JS использовать
         }),
         new HtmlWebpackPlugin({
           filename: 'login.html',
